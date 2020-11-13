3796e5ac8b8888822f781ab0f2ebca4efba5cc8c
Kiril Zafirov <kiril.zafirov@it-labs.com>
2020-11-13 21:53:26 +0100

    bump angular cli version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, ExtraOptions } from '@angular/router';
 import { AuthGuard } from './core/guards/auth.guard';
-import { QuicklinkModule, QuicklinkStrategy } from 'ngx-quicklink'; 
+import { QuicklinkModule, QuicklinkStrategy } from 'ngx-quicklink';
 
 const routerOptions: ExtraOptions = {
   enableTracing: true,
@@ -9,58 +9,76 @@ const routerOptions: ExtraOptions = {
   scrollPositionRestoration: 'enabled',
   anchorScrolling: 'enabled',
   scrollOffset: [0, 32],
-  preloadingStrategy: QuicklinkStrategy 
+  preloadingStrategy: QuicklinkStrategy,
+  relativeLinkResolution: 'legacy',
 };
 
 const routes: Routes = [
-    {
-      path: 'products',
-      loadChildren: () => import('./features/products/products-handler.module').then(m => m.ProductsHandlerModule),
-      canActivate: [AuthGuard]
-    },
-    {
-      path: 'details/:id',
-      loadChildren: () => import('./features/details-page/details-page.module').then(m => m.DetailsPageModule),
-      canActivate: []
-    }, 
-    {
-      path: '404',
-      loadChildren:() =>import('./features/not-found/not-found.module').then(m => m.NotFoundModule) ,
-    },
-    {
-      path: 'forbidden',
-      loadChildren:() =>import('./features/forbidden/forbidden.module').then(m => m.ForbiddenModule) ,
-    },
-    {
-      path: 'login',
-      loadChildren:() =>import('./features/auth/auth.module').then(m => m.AuthModule) ,
-      pathMatch: 'full',
-    },
-    {
-      path: 'user',
-      loadChildren:() =>import('./features/user-settings/user-settings.module').then(m => m.UserSettingsModule),
-    },
-    {
-      path: 'home',
-      loadChildren:() =>import('./features/landing-page/landing-page.module').then(m => m.LandingPageModule) ,
-      pathMatch: 'full',
-    },
-    {
-      path: "", 
-      redirectTo:'/home',
-      pathMatch: 'full'
-    },
-    {
-      path: "**",
-      pathMatch: "full",
-      redirectTo:'/404'
-    }
-  ];
+  {
+    path: 'products',
+    loadChildren: () =>
+      import('./features/products/products-handler.module').then(
+        (m) => m.ProductsHandlerModule
+      ),
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'details/:id',
+    loadChildren: () =>
+      import('./features/details-page/details-page.module').then(
+        (m) => m.DetailsPageModule
+      ),
+    canActivate: [],
+  },
+  {
+    path: '404',
+    loadChildren: () =>
+      import('./features/not-found/not-found.module').then(
+        (m) => m.NotFoundModule
+      ),
+  },
+  {
+    path: 'forbidden',
+    loadChildren: () =>
+      import('./features/forbidden/forbidden.module').then(
+        (m) => m.ForbiddenModule
+      ),
+  },
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./features/auth/auth.module').then((m) => m.AuthModule),
+    pathMatch: 'full',
+  },
+  {
+    path: 'user',
+    loadChildren: () =>
+      import('./features/user-settings/user-settings.module').then(
+        (m) => m.UserSettingsModule
+      ),
+  },
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./features/landing-page/landing-page.module').then(
+        (m) => m.LandingPageModule
+      ),
+    pathMatch: 'full',
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: '/404',
+  },
+];
 
 @NgModule({
-  imports: [
-    QuicklinkModule,
-    RouterModule.forRoot(routes , routerOptions)],
-  exports: [RouterModule]
+  imports: [QuicklinkModule, RouterModule.forRoot(routes, routerOptions)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
